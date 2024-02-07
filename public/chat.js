d414a646e194c4854ead366fb3f29baee877aa07
sencerarslan <m.sencerarslan@gmail.com>
2024-02-07 14:10:32 +0300

    vercel host url

--- public/chat.js
+++ public/chat.js
@@ -1,4 +1,4 @@
-const socket = io.connect("http://localhost:3000");
+const socket = io.connect("https://socketio-chat-app-orcin.vercel.app");
 
 const sender = document.getElementById("sender");
 const message = document.getElementById("message");
